9dc927df33fc6a9ee8183dd67cc1c59bffce198c
Janaka Stevens <github@calitek.com>
2016-05-02 11:09:00 -0700

    Tabs to spaces

--- React.15/ReFluxWebSocket/js/socket/GetSetData.js
+++ React.15/ReFluxWebSocket/js/socket/GetSetData.js
@@ -5,25 +5,24 @@ var fs = require('fs');
 var rootDataPath = './data';
 
 var getData = function(doneCallBack) {
-	var filePath = rootDataPath + '/basic.json';
-	var jsonReadCallBack = function(err, data){
-		if (err) doneCallBack('Data readFile error ' + filePath);
-		else {
-			var jsonData = JSON.parse(data.toString());
-			doneCallBack(jsonData);
-		}
-	};
-	fs.readFile(filePath, jsonReadCallBack);
+  var filePath = rootDataPath + '/basic.json';
+  var jsonReadCallBack = function(err, data){
+    if (err) doneCallBack('Data readFile error ' + filePath);
+    else {
+      var jsonData = JSON.parse(data.toString());
+      doneCallBack(jsonData);
+    }
+  };
+  fs.readFile(filePath, jsonReadCallBack);
 };
 
 var setData = function(data) {
-	var filePath = rootDataPath + '/basic.json';
-	var writeFileCallBack = function (err) {
-		if (err) console.log('error saving Data.json file ');
-	};
-	fs.writeFile(filePath, JSON.stringify(data, null, 2), writeFileCallBack);
+  var filePath = rootDataPath + '/basic.json';
+  var writeFileCallBack = function (err) {
+    if (err) console.log('error saving Data.json file ');
+  };
+  fs.writeFile(filePath, JSON.stringify(data, null, 2), writeFileCallBack);
 };
 
 module.exports.getData = getData;
 module.exports.setData = setData;
-
