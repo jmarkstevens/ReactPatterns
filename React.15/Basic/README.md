3f55e83e48b49cffd09fd86efaa6b9e34cf87f51
Janaka Stevens <github@calitek.com>
2016-05-27 11:35:20 -0700

    Added unit test to React.15.Basic.

--- React.15/Basic/README.md
+++ React.15/Basic/README.md
@@ -6,3 +6,4 @@ Just the basics for react 15.
 
 cd into the root directory of the project. If the first time "npm install". "npm run ww" for webpack with watch.
 "npm run nw" for webpack no watch. "npm start" to start the node server. "localhost:3500" in the browser.
+"npm test" for unit testing with Jest and Enzyme.
---
+++ React.15/Basic/__tests__/app.ctrl.test.js
@@ -0,0 +1,33 @@
+"use strict";
+
+jest.unmock('../ui-src/components/app.ctrl');
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import AppCtrl from '../ui-src/components/app.ctrl';
+
+describe('<AppCtrl />', () => {
+  it('checks div count', () => {
+    const wrapper = shallow(<AppCtrl />);
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+  it('checks br count', () => {
+    const wrapper = shallow(<AppCtrl />);
+    expect(wrapper.find('br').length).toEqual(2);
+  });
+  it('checks shallow text', () => {
+    const wrapper = shallow(<AppCtrl />);
+    expect(wrapper.text()).toEqual('React 15 Basic');
+  });
+  it('checks mounted text', () => {
+    const wrapper = mount(<AppCtrl />);
+    expect(wrapper.text()).toEqual('React 15 BasicHello World');
+  });
+  it('checks shallow state', () => {
+    const wrapper = shallow(<AppCtrl />);
+    expect(wrapper.state('world')).toEqual('');
+  });
+  it('checks mounted state', () => {
+    const wrapper = mount(<AppCtrl />);
+    expect(wrapper.state('world')).toEqual('Hello World');
+  });
+});
